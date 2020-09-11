fda9de100b6d7bb1ea2609a7b8e115116a319276
Mallone <mallonenogueira@gmail.com>
2020-09-11 01:03:33 -0300

    create importmap plugin

--- plugins/fetch-importmaps/index.js
+++ plugins/fetch-importmaps/index.js
@@ -41,9 +41,14 @@ module.exports = {
       },
     };
 
+    console.log("PARAMS: ", params);
+
     if (IMPORTMAP_AUTH) {
+      console.log("IMPORTMAP_AUTH: ", IMPORTMAP_AUTH);
       options.headers.authorization = IMPORTMAP_AUTH;
     }
+
+    console.log("IMPORTMAP_URL: ", IMPORTMAP_URL);
 
     await api(
       Object.assign(options, parse(IMPORTMAP_URL, options)),
