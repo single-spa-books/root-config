bf40c9d54300a2ef32b8911352dd95988be7aab7
Mallone <mallonenogueira@gmail.com>
2020-09-12 13:58:36 -0300

    feat: change @single-spa/welcome by @single-spa/navbar

--- src/single-spa-books-root-config.js
+++ src/single-spa-books-root-config.js
@@ -1,19 +1,10 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@single-spa-books/navbar",
+  app: () => System.import("@single-spa-books/navbar"),
   activeWhen: ["/"],
 });
-
-// registerApplication({
-//   name: "@single-spa-books/navbar",
-//   app: () => System.import("@single-spa-books/navbar"),
-//   activeWhen: ["/"]
-// });
 
 start({
   urlRerouteOnly: true,
