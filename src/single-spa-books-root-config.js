2dbd7c773832d611230a4568d72a104ab6911619
Mallone <mallonenogueira@gmail.com>
2020-09-21 21:44:31 -0300

    feat: add loading application and loading modules

--- src/single-spa-books-root-config.js
+++ src/single-spa-books-root-config.js
@@ -1,21 +1,58 @@
 import { registerApplication, start } from "single-spa";
+import { createRoutes } from "./routes";
+import Loading from "./loading";
+
+const loadingEl = document.getElementById("application-loading");
+
+const loading = (
+  fn,
+  props = {
+    text: "Loading module",
+    classNames: "loading-module",
+  }
+) => {
+  return (args) => {
+    const { mountParcel } = args;
+
+    const loadingParcel = mountParcel(new Loading(), {
+      domElement: loadingEl,
+      props,
+    });
+    const response = fn(args);
+
+    return new Promise((resolve) => {
+      setTimeout(async () => {
+        loadingParcel.unmount();
+        resolve(await response);
+      }, 1000);
+    });
+  };
+};
+
+const exactPath = (path) => ({ pathname }) => pathname === path;
+const registerApplications = (routes) => routes.forEach(registerApplication);
 
 registerApplication({
   name: "@single-spa-books/navbar",
-  app: () => System.import("@single-spa-books/navbar"),
+  app: loading(
+    async () => {
+      const navbar = await System.import("@single-spa-books/navbar");
+
+      const routes = createRoutes({
+        exactPath,
+        loading,
+      });
+
+      registerApplications(routes);
+
+      return navbar;
+    },
+    {
+      text: "Loading application",
+      classNames: "loading-application",
+    }
+  ),
   activeWhen: ["/"],
-});
-
-registerApplication({
-  name: "@single-spa-books/home",
-  app: () => System.import("@single-spa-books/home"),
-  activeWhen: ({ pathname }) => pathname === "/",
-});
-
-registerApplication({
-  name: "@single-spa-books/books",
-  app: () => System.import("@single-spa-books/books"),
-  activeWhen: ["/books"],
 });
 
 start({
